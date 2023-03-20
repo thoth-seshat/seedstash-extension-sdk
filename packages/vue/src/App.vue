1fd1442a0f39c732f16e7ba15851dd1bfa22d1f8
Sterling Cobb <sterling@seedstash.xyz>
2023-03-20 11:54:34 -0600

    init vue and react mono repo

---
+++ packages/vue/src/App.vue
@@ -0,0 +1,42 @@
+<!-- App.vue (or your main component) -->
+<template>
+  <div>
+    <button @click="toggleModal">Open Modal</button>
+    <div v-if="isModalOpen" class="modal">
+      <button @click="toggleModal">Close Modal</button>
+      <iframe
+        title="Walkthrough"
+        src="https://example.com/walkthrough-url"
+        width="100%"
+        height="100%"
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isModalOpen: false,
+    };
+  },
+  name: "App",
+  methods: {
+    toggleModal() {
+      this.isModalOpen = !this.isModalOpen;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
